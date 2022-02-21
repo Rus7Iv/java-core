d1e09da124c50e268761627790cdb4ef376c6506
Rus7Iv <rusivary@gmail.com>
2022-02-21 10:19:55 +0400

    Lesson 2 (file "Variable.java")

---
+++ src/Variable.java
@@ -0,0 +1,17 @@
+public class Variable {
+    public static void main(String[] args){
+        int myInt;
+        myInt = 557;
+        short myShort = 3266;
+        long myLong = 1223243;
+
+        double myDouble = 235.6;
+        float myFloat = 2365.3f;
+
+        char c = 'a';
+        boolean b = true;
+
+        byte myByte = 100; // -128-128
+        System.out.println(myInt);
+    }
+}
