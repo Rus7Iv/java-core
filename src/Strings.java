3e0beab5bfb13c5dbf080ed3dd7333d32f8662ba
Rus7Iv <rusivary@gmail.com>
2022-02-21 11:02:51 +0400

    Lesson 3 (file "String.java")

--- src/Strings.java
+++ src/Strings.java
@@ -1,8 +1,11 @@
 public class Strings {
     public static void main(String[] args){
+        int x = 5;
         java.lang.String s = "Hello";
         java.lang.String space = " ";
         java.lang.String name = "Bob";
         System.out.println(s+space+name);
+        System.out.println("Hello"+" "+"John");
+        System.out.println("My number is "+x);
     }
 }
