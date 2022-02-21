d078910259a8c8c25d820d9b880bddb6485ee248
Rus7Iv <rusivary@gmail.com>
2022-02-21 11:42:54 +0400

    Lesson 7 (file "Input.java")

---
+++ src/Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите что-нибудь: ");
+        String string = s.nextLine();
+        System.out.println("Введите какое-нибудь число: ");
+        int myInt = s.nextInt();
+        System.out.println("Вы ввели "+string+" "+myInt);
+    }
+}
