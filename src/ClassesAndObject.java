45c8f24d5173645db9dbe66c832ac4a857a7287a
Rus7Iv <rusivary@gmail.com>
2022-02-22 10:38:49 +0400

    Lesson 16

--- src/ClassesAndObject.java
+++ src/ClassesAndObject.java
@@ -3,13 +3,19 @@ public class ClassesAndObject {
         Person person1 = new Person();
         person1.name = "Roman";
         person1.age = 13;
-        person1.sayHello();
-        person1.speak();
+        //person1.sayHello();
+        //person1.speak();
 
         Person person2 = new Person();
         person2.name = "Vladimir";
         person2.age = 20;
-        person2.speak();
+       //person2.speak();
+
+        int year1 = person1.calculateYearsToRetirement();
+        int year2 = person2.calculateYearsToRetirement();
+
+        System.out.println("Количество лет до пенсии первому человеку - "+year1);
+        System.out.println("Количество лет до пенсии второму человеку - "+year2);
     }
 }
 
@@ -19,6 +25,12 @@ class Person{
     //2. Действия, которые он может соыершать (методы)
     String name;
     int age;
+    int calculateYearsToRetirement() {
+        int years = 65 - age;
+        return years;
+        //System.out.println("Количество лет до пенсии - "+years);
+    }
+
     void speak(){
         for(int i = 0; i<3; i++) {
             System.out.println("Меня зовут " + name + ", мне " + age + " лет");
